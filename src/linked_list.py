0dbe3771141dd5eeba9649cd59c7d3ae21ba55ae
Carlos Cadena <cs.cadena@gmail.com>
2017-05-23 16:40:52 -0700

    linked list functioning

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,27 +1,27 @@
 class LinkedList(object):
-    
     def __init__(self, data):
         self.data = None
         self.next_node = None
         self.head = self
         flag = 0
-        if type(data) == list or type(data) == tuple:
+        if type(data) in [list, tuple, str]:
             for x in range(len(data)):
-                if flag ==0:
+                if flag == 0:
                     self.data = data[x]
                     self.next_node = None
-                    flag =1
-                
+                    flag = 1
+
                 else:
                     self.push(data[x])
         else:
             self.data = data
+
     def push(self, val):
         current = self.head
         newl = LinkedList(val)
         newl.next_node = current
         self.head = newl
-        return self
+        return self.head
 
     def pop(self):
         current = self.head
@@ -32,19 +32,19 @@ class LinkedList(object):
             return tem_data
         except AttributeError:
             flag = 0
-        raise IndexError ('linked list is empty')
+        raise IndexError('linked list is empty')
 
     def size(self):
         current = self.head
         count = 1
-        while current.next_node:
+        while current.next_node is not None:
             count += 1
             current = current.next_node
         return count
 
     def search(self, val):
         current = self.head
-        while current.next_node != None:
+        while current.next_node is not None:
             if current.data == val:
                 return current
             else:
@@ -58,26 +58,35 @@ class LinkedList(object):
     def remove(self, node):
         current = self.head
         previouscurrent = None
+        if node == current:
+            self.head = current.next_node
+            return self.head
         while current.next_node is not None:
             if current == node:
                 previouscurrent.next_node = current.next_node
-                return
+                return self.head
             else:
                 previouscurrent = current
                 current = current.next_node
-            
+        if node == current:
+            previouscurrent.next_node = None
+            return self.head
+
         raise Exception('Node does not exist in Linked List')
 
     def display(self):
         current = self.head
         output = ''
-        while current.next_node != None:
+        while current.next_node is not None:
             output += str(current.data) + ', '
             current = current.next_node
         output += str(current.data)
         return output
-new = LinkedList([1,2,3])
-new.remove(new.search(2))
-print(new.display())
 
+    def __len__(self):
+        length = self.size()
+        return length
 
+    def __repr__(self):
+        output_list = self.head.display()
+        return output_list
