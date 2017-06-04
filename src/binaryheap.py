4150efdf761c8ec75f2c1dfcd87e28c11654e557
SeanBeseler <seanwbeseler@gmail.com>
2017-06-04 16:01:14 -0700

    binary heap done

--- src/binaryheap.py
+++ src/binaryheap.py
@@ -1,70 +1,81 @@
-
-class binary_heap(object):
-    def __init__(self):
+class Binary_heap(object):
+    def __init__(self, val = None):
+        """init binary heap and checks val"""
         self.size = 0
         self.list = []
+        if type(val) in [list, tuple]:
+            for x in val:
+                self.push(x)
 
-    def _get_parent(self, position):
-        if position % 2 == 1:
-            position -= 1
-        parent_position = position/2
+def _get_parent(self, position):
+    """returns the parent of the position"""
+        position -= 1
+        parent_position = int(position/2)
         return parent_position
 
-    def _check_heap_after_push(self):
+    def _get_left_kid(self, position):
+        """gets the left kid for a position"""
+        position = (position *2) + 1
+        if position > self.size -1:
+            return None
+        return position
+
+def _get_right_kid(self, position):
+    """gets the right kid for a position"""
+        position = (position +1) * 2
+        if position > self.size -1:
+            return None
+    return position
+
+def _check_heap_after_push(self):
+    """gets the left kid for a position"""
         for x in range(self.size -1):
             current_position = self.size - x -1
-            #if current_position < 3:
             if self.list[current_position] < self.list[self._get_parent(current_position)]:
                 temp_var = self.list[current_position]
                 self.list[current_position] = self.list[self._get_parent(current_position)]
                 self.list[self._get_parent(current_position)] = temp_var
-            """else:
-                partent1 = 0
-                partent2 = 0
-                if self._get_parent(current_position) %2 == 0:
-                    partent1 = self._get_parent(current_position) -1
-                    partent2 = self._get_parent(current_position)
-                else:
-                    partent1 = self._get_parent(current_position) 
-                    partent2 = self._get_parent(current_position) + 1
                     
-                if partent1 > partent2:
-                    if self.list[current_position] < self.list[partent1]:
-                        temp_var = self.list[current_position]
-                        self.list[current_position] = self.list[partent1]
-                        self.list[partent1] = temp_var
-                else:
-                    if self.list[current_position] < self.list[partent2]:
-                        temp_var = self.list[current_position]
-                        self.list[current_position] = self.list[partent2]
-                        self.list[partent2] = temp_var"""
-                    
-    def push(self,val):
-        if val not in self.list:
-            self.list.append(val)
-            self.size += 1
+                    def push(self,val):
+                        """push a val on to the list"""
+if val not in self.list:
+    self.list.append(val)
+        self.size += 1
             self._check_heap_after_push()
-            
-    def _check_heap_after_pop(self):
-        for x in range(self.size -1):
-            if x + 2 < self.size:
-                if self.list[x] > self.list[(x*2)+ 1] or self.list[x] > self.list[(x*2) + 2]:
-                    if self.list[(x*2)+1] < self.list[(x*2)+2]:
-                        temp_var = self.list[x]
-                        self.list[x] = self.list[x+1]
-                        self.list[x+1] = temp_var
-                    else:
-                        temp_var = self.list[x]
-                        self.list[x] = self.list[x+2]
-                        self.list[x+2] = temp_var
-            
-                        
-                elif self.list[x] > self.list[x+1]:
-                        temp_var = self.list[x]
-                        self.list[x] = self.list[x+1]
-                        self.list[x+1] = temp_var
+        else:
+            raise ValueError (' number in heap allready')
 
-    def pop(self):
+def _check_heap_after_pop(self):
+    """sorts the hep after a pop"""
+        for x in range(self.size -1):
+            current_position = x
+            right_kid = self._get_right_kid(current_position)
+            left_kid = self._get_left_kid(current_position)
+            if right_kid is not None or left_kid is not None:
+                if right_kid is None:
+                    if self.list[left_kid] < self.list[current_position]:
+                        temp_var = self.list[current_position]
+                        self.list[current_position] = self.list[left_kid]
+                        self.list[left_kid] = temp_var
+                elif left_kid is None:
+                    if self.list[right_kid] < self.list[current_position]:
+                        temp_var = self.list[current_position]
+                        self.list[current_position] = self.list[right_kid]
+                        self.list[right_kid] = temp_var
+                else:
+                    if self.list[left_kid] < self.list[current_position] and self.list[left_kid] < self.list[right_kid]:
+                        temp_var = self.list[current_position]
+                        self.list[current_position] = self.list[left_kid]
+                        self.list[left_kid] = temp_var
+                    elif self.list[right_kid] < self.list[current_position] and self.list[left_kid] > self.list[right_kid]:
+                        temp_var = self.list[current_position]
+                        self.list[current_position] = self.list[right_kid]
+                        self.list[right_kid] = temp_var
+    
+
+
+def pop(self):
+    """pop the lowest value off"""
         tem_var = self.list[0]
         self.list[0] = self.list[self.size -1]
         self.list[self.size -1] = tem_var
@@ -72,32 +83,7 @@ class binary_heap(object):
         self.size -= 1
         self._check_heap_after_pop()
         return tem_var
-    
-bh = binary_heap()
-bh.push(13)
-print(bh.list)
-bh.push(9)
-print(bh.list)
-bh.push(15)
-print(bh.list)
-bh.push(12)
-print(bh.list)
-bh.push(11)
-print(bh.list)
-bh.push(14)
-print(bh.list)
-bh.push(11.1)
-print(bh.list)
-print(bh.pop())
-print(bh.list)
-print(bh.pop())
-print(bh.list)
-print(bh.pop())
-print(bh.list)
-print(bh.pop())
-print(bh.list)
-print(bh.pop())
-print(bh.list)
-print(bh.pop())
 
-                   
+
+
+
